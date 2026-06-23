768ab96e8e0060777ff5dfa5f050131db82bb380
akashcoder02 <akashgoyalit1998@gmail.com>
2026-06-23 13:05:01 +0000

    Added AKS deployment pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,28 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "akashdevopsacr12345"
+  name                = "agdevopsacr2026"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
   sku           = "Basic"
   admin_enabled = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-devops-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  dns_prefix = "aksdevopsdemo"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,7 @@ output "acr_name" {
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
+
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
