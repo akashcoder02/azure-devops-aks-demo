4e301dd6c5096fbe4a3b7f227b13a53de56dacf8
akashcoder02 <akashgoyalit1998@gmail.com>
2026-07-01 18:12:26 +0000

    Add Terraform-managed NGINX Ingress Controller and update service to ClusterIP

--- terraform/modules/aks/outputs.tf
+++ terraform/modules/aks/outputs.tf
@@ -5,3 +5,21 @@ output "aks_name" {
 output "kubelet_object_id" {
   value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
+output "host" {
+  value = azurerm_kubernetes_cluster.aks.kube_config[0].host
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "client_key" {
+  value = azurerm_kubernetes_cluster.aks.kube_config[0].client_key
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value = azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate
+  sensitive = true
+}
