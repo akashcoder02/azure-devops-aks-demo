403a1d8040dc35351f217d3f2682ea79d54f8b65
akashcoder02 <akashgoyalit1998@gmail.com>
2026-07-02 20:29:59 +0000

    Revert Flask ingress changes

--- applications/tic-tac-toe/app.py
+++ applications/tic-tac-toe/app.py
@@ -1,21 +1,10 @@
 from flask import Flask, render_template
-from werkzeug.middleware.proxy_fix import ProxyFix
 
-app = Flask(
-    __name__,
-    static_url_path="/tic-tac-toe/static"
-)
-
-app.wsgi_app = ProxyFix(app.wsgi_app)
-
-app.wsgi_app = ProxyFix(app.wsgi_app)
-app.config["APPLICATION_ROOT"] = "/tic-tac-toe"
-
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
