31611d140e118c2a38b4be2b9905b4e074f71552
akashcoder02 <akashgoyalit1998@gmail.com>
2026-07-02 20:11:09 +0000

    Support ingress path prefix

--- applications/tic-tac-toe/app.py
+++ applications/tic-tac-toe/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+app.config["APPLICATION_ROOT"] = "/tic-tac-toe"
 
 
 @app.route("/")
