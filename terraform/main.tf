7366fb5bcdd44beebddba00007ec94b8773df474
akashcoder02 <akashgoyalit1998@gmail.com>
2026-07-06 16:49:11 +0000

    CHanged

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,6 +26,8 @@ resource "azurerm_role_assignment" "aks_acr_pull" {
   role_definition_name = "AcrPull"
   principal_id         = module.aks.kubelet_object_id
 
+  skip_service_principal_aad_check = true
+
   depends_on = [
     module.aks,
     module.acr
