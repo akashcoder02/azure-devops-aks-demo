8e8bc616ba8242957beade8f63c40bc9d9429ee2
akashcoder02 <akashgoyalit1998@gmail.com>
2026-07-21 13:41:42 +0000

    711

--- app/services/applications.py
+++ app/services/applications.py
@@ -1,6 +1,40 @@
+import subprocess
+
+
 class ApplicationsService:
 
+    def get_public_ip(self):
+
+        try:
+
+            result = subprocess.run(
+
+                [
+                    "kubectl",
+                    "get",
+                    "svc",
+                    "ingress-nginx-controller",
+                    "-n",
+                    "ingress-nginx",
+                    "-o",
+                    "jsonpath={.status.loadBalancer.ingress[0].ip}"
+                ],
+
+                capture_output=True,
+                text=True,
+                check=True
+
+            )
+
+            return result.stdout.strip()
+
+        except Exception:
+
+            return ""
+
     def get_applications(self):
+
+        public_ip = self.get_public_ip()
 
         return [
 
@@ -12,7 +46,17 @@ class ApplicationsService:
                 "deployment": "GitOps",
                 "pods": "2/2",
                 "hpa": "Enabled",
-                "url": "http://localhost:8080/tic-tac-toe"
+
+                "local_url":
+                    "http://localhost:8080/tic-tac-toe",
+
+                "public_url":
+                    (
+                        f"http://{public_ip}/tic-tac-toe"
+                        if public_ip
+                        else "#"
+                    )
+
             },
 
             {
@@ -23,7 +67,17 @@ class ApplicationsService:
                 "deployment": "Traditional",
                 "pods": "1/1",
                 "hpa": "Disabled",
-                "url": "http://localhost:8080/tetris"
+
+                "local_url":
+                    "http://localhost:8080/tetris",
+
+                "public_url":
+                    (
+                        f"http://{public_ip}/tetris"
+                        if public_ip
+                        else "#"
+                    )
+
             }
 
         ]
