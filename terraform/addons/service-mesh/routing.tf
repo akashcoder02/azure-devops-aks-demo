290646e927294a405769ad2926468d6c8c7d2bd1
akashcoder02 <akashgoyalit1998@gmail.com>
2026-08-03 13:17:02 +0530

    117

--- terraform/addons/service-mesh/routing.tf
+++ terraform/addons/service-mesh/routing.tf
@@ -49,61 +49,41 @@ resource "kubectl_manifest" "virtualservice" {
 
           timeout = "5s"
 
-          route = concat(
+          route = [
 
-            [
-              {
-                destination = {
-                  host   = each.key
-                  subset = each.value.primary.version
+            {
+              destination = {
+                host   = each.key
+                subset = each.value.primary.version
 
-                  port = {
-                    number = each.value.service_port
-                  }
+                port = {
+                  number = each.value.service_port
                 }
-
-                weight = (
-                  (each.key == var.traffic_application &&
-                  var.primary_weight_override >= 0)
-                  ? var.primary_weight_override
-                  : (
-                      each.value.canary_enabled
-                      ? each.value.primary.weight
-                      : 100
-                    )
-                )
-              }
-            ],
-
-            (
-              (
-                each.key == var.traffic_application
-                ? var.canary_enabled_override
-                : each.value.canary_enabled
-              )
-            ) ? [
-
-              {
-                destination = {
-                  host   = each.key
-                  subset = each.value.canary.version
-
-                  port = {
-                    number = each.value.service_port
-                  }
-                }
-
-                weight = (
-                  (each.key == var.traffic_application &&
-                  var.canary_weight_override >= 0)
-                  ? var.canary_weight_override
-                  : each.value.canary.weight
-                )
               }
 
-            ] : []
+              weight = (
+                each.key == var.traffic_application &&
+                var.primary_weight_override >= 0
+              ) ? var.primary_weight_override : each.value.primary.weight
+            },
 
-          )
+            {
+              destination = {
+                host   = each.key
+                subset = each.value.canary.version
+
+                port = {
+                  number = each.value.service_port
+                }
+              }
+
+              weight = (
+                each.key == var.traffic_application &&
+                var.canary_weight_override >= 0
+              ) ? var.canary_weight_override : each.value.canary.weight
+            }
+
+          ]
 
         }
       ]
