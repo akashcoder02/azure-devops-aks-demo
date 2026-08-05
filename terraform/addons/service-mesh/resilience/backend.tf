3e3f3ce124e8d6e18c1563886672b16fa96227d2
akashcoder02 <akashgoyalit1998@gmail.com>
2026-08-05 20:18:53 +0530

    818

--- terraform/addons/service-mesh/resilience/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-tf-backend"
-    storage_account_name = "agtfstate2026xyz"
-    container_name       = "tfstate"
-    key                  = "service-mesh.tfstate"
-  }
-}
