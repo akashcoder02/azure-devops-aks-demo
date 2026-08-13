70a0b11ad3d57a0df9da0dd330ed732b5f4ecf3b
akashcoder02 <akashgoyalit1998@gmail.com>
2026-08-13 18:47:51 +0530

    647

--- terraform/addons/service-mesh/routing.tf
+++ terraform/addons/service-mesh/routing.tf
@@ -24,68 +24,102 @@ resource "kubectl_manifest" "virtualservice" {
       ]
 
       http = [
-        {
 
-          match = [
-            {
-              uri = {
-                regex = "^/${each.key}(/|$)(.*)"
-              }
-            }
-          ]
+        merge(
 
-          rewrite = {
-            uriRegexRewrite = {
-              match   = "^/${each.key}(/|$)(.*)"
-              rewrite = "/\\2"
-            }
-          }
-
-          retries = {
-            attempts      = var.retry_attempts
-            perTryTimeout = var.per_try_timeout
-            retryOn       = "gateway-error,connect-failure,refused-stream,5xx"
-          }
-
-          timeout = var.request_timeout
-
-          route = [
-
-            {
-              destination = {
-                host   = each.key
-                subset = each.value.primary.version
-
-                port = {
-                  number = each.value.service_port
+          {
+            match = [
+              {
+                uri = {
+                  regex = "^/${each.key}(/|$)(.*)"
                 }
               }
+            ]
 
-              weight = (
-                each.key == var.traffic_application &&
-                var.primary_weight_override >= 0
-              ) ? var.primary_weight_override : each.value.primary.weight
-            },
-
-            {
-              destination = {
-                host   = each.key
-                subset = each.value.canary.version
-
-                port = {
-                  number = each.value.service_port
-                }
+            rewrite = {
+              uriRegexRewrite = {
+                match   = "^/${each.key}(/|$)(.*)"
+                rewrite = "/\\2"
               }
-
-              weight = (
-                each.key == var.traffic_application &&
-                var.canary_weight_override >= 0
-              ) ? var.canary_weight_override : each.value.canary.weight
             }
 
-          ]
+            retries = {
+              attempts      = var.retry_attempts
+              perTryTimeout = var.per_try_timeout
+              retryOn       = "gateway-error,connect-failure,refused-stream,5xx"
+            }
 
-        }
+            timeout = var.request_timeout
+
+            route = [
+
+              {
+                destination = {
+                  host   = each.key
+                  subset = each.value.primary.version
+
+                  port = {
+                    number = each.value.service_port
+                  }
+                }
+
+                weight = (
+                  each.key == var.traffic_application &&
+                  var.primary_weight_override >= 0
+                ) ? var.primary_weight_override : each.value.primary.weight
+              },
+
+              {
+                destination = {
+                  host   = each.key
+                  subset = each.value.canary.version
+
+                  port = {
+                    number = each.value.service_port
+                  }
+                }
+
+                weight = (
+                  each.key == var.traffic_application &&
+                  var.canary_weight_override >= 0
+                ) ? var.canary_weight_override : each.value.canary.weight
+              }
+
+            ]
+          },
+
+          (
+            var.fault_delay_enabled || var.fault_abort_enabled
+          ) ? {
+
+            fault = merge(
+
+              var.fault_delay_enabled ? {
+                delay = {
+                  percentage = {
+                    value = var.fault_delay_percentage
+                  }
+
+                  fixedDelay = var.fault_delay
+                }
+              } : {},
+
+              var.fault_abort_enabled ? {
+                abort = {
+                  percentage = {
+                    value = var.fault_abort_percentage
+                  }
+
+                  httpStatus = var.fault_abort_status
+                }
+              } : {}
+
+            )
+
+          } : {}
+
+        )
+
       ]
 
     }
